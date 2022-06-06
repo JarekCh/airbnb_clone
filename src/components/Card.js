e64c0e305d08f96d40f63847c200dd733ff8a003
Jaroslaw Charchula <charchula@gmail.com>
2022-06-06 21:57:35 +0200

    set props for card and app components.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
 function Card(props) {
-  return (
-    <div className='card'>
-        <img src={require(`../img/${props.img}`)} className='card__img'/>
-        <div className='card__stats'>
-            <img src={require(`../img/Star.png`)} className='card__star'/>
-            <span>{props.rating}</span>
-            <span className='gray'>({props.reviewCount}) • </span>
-            <span className='gray'>{props.location}</span> 
+    let badgeText
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT"        
+    } else if(props.item.location === "Online") {
+        badgeText = "Online"  
+    }
+
+    return (
+        <div className='card'>
+            {(props.item.openSpots === 0 
+                || props.item.location === "Online") 
+                &&  <div className='card__badge'>{badgeText}</div>}
+            <img src={require(`../img/${props.item.coverImg}`)} className='card__img'/>
+            <div className='card__stats'>
+                <img src={require(`../img/Star.png`)} className='card__star'/>
+                <span>{props.item.stats.rating}</span>
+                <span className='gray'>({props.item.stats.reviewCount}) • </span>
+                <span className='gray'>{props.item.location}</span> 
+            </div>
+            <p className='card__title'>{props.item.title}</p>
+            <p className='card__price'><span className='bold'>From ${props.item.price}</span> / person</p>
         </div>
-        <p className='card__title'>{props.title}</p>
-        <p className='card__price'><span className='bold'>From ${props.price}</span> / person</p>
-    </div>
   )
 }
 
